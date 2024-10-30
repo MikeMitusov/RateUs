1b99be4c86634d15c99150671ddc5e60d32ee5bf
MikeMitusov <mitsivimail@gmail.com>
2024-10-30 10:16:10 +0200

    [Rate app] Create models

--- mysite/rate/models.py
+++ mysite/rate/models.py
@@ -1,3 +1,32 @@
+from django.core.validators import MaxValueValidator, MinValueValidator
 from django.db import models
 
-# Create your models here.
+
+class Author(models.Model):
+    name = models.CharField(max_length=40)
+    email = models.EmailField()
+    avatar = models.ImageField(upload_to="uploads/%Y/%m/%d/")
+    bio = models.CharField(max_length=150)
+    allow_mail = models.BooleanField(default=True)
+    created = models.DateTimeField(auto_now_add=True)
+    updated = models.DateTimeField(auto_now=True)
+    active = models.BooleanField(default=True)
+
+
+class Link(models.Model):
+    author = models.ForeignKey(Author, on_delete=models.CASCADE, related_name="links")
+    title = models.CharField(max_length=20)
+    url = models.URLField()
+    active = models.BooleanField(default=True)
+
+
+class Review(models.Model):
+    author = models.ForeignKey(Author, on_delete=models.CASCADE, related_name="reviews")
+    rating = models.IntegerField(
+        validators=[MaxValueValidator(5), MinValueValidator(1)]
+    )
+    name = models.CharField(max_length=40)
+    email = models.EmailField()
+    body = models.TextField()
+    active = models.BooleanField(default=True)
+    created = models.DateTimeField(auto_now_add=True)
