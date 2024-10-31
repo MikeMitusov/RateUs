79933e6ca1e7afa5ad141c8a70cd9e9970268472
MikeMitusov <mitsivimail@gmail.com>
2024-10-31 10:56:44 +0200

    [Rate app] Add author detail.html

--- mysite/rate/views.py
+++ mysite/rate/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render, get_object_or_404
 from django.views.generic import ListView
 from .models import Author, Link, Review
+from django.core.paginator import Paginator, PageNotAnInteger, EmptyPage
 
 
 class AuthorListView(ListView):
@@ -13,5 +14,16 @@ class AuthorListView(ListView):
 
 def author_detail(request, author_id):
     author = get_object_or_404(Author, active=True, id=author_id)
+    reviews = Review.objects.filter(active=True, author=author)
+    reviews = Paginator(reviews, 3)
 
-    return render(request, "rate/author/detail.html", {"author": author})
+    try:
+        page_obj = reviews.page(request.GET.get("page", 1))
+    except PageNotAnInteger:
+        page_obj = reviews.page(1)
+    except EmptyPage:
+        page_obj = reviews.page(1)
+
+    return render(
+        request, "rate/author/detail.html", {"author": author, "reviews": page_obj}
+    )
