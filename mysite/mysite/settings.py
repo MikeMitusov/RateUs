0641f0da0a910a7b3e2a0d5e1cee6f1e2a321a0a
MikeMitusov <mitsivimail@gmail.com>
2024-11-01 10:09:07 +0200

    Install django-extensions

--- mysite/mysite/settings.py
+++ mysite/mysite/settings.py
@@ -12,7 +12,6 @@ https://docs.djangoproject.com/en/5.1/ref/settings/
 
 from pathlib import Path
 from decouple import config
-from django.conf.global_settings import EMAIL_HOST, EMAIL_HOST_USER
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -40,6 +39,7 @@ INSTALLED_APPS = [
     "django.contrib.messages",
     "django.contrib.staticfiles",
     "rate.apps.RateConfig",
+    "django_extensions",
 ]
 
 MIDDLEWARE = [
